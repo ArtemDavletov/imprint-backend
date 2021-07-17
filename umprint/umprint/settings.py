304b3da9a4d430255b5cffed4868445cfb3265db
Artem Davletov <Tomas-Cooper@mail.ru>
2021-07-17 19:22:20 +0300

    WIP: update settings.py

--- umprint/umprint/settings.py
+++ umprint/umprint/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        # 'HOST': 'pgdb',
-        "HOST": "localhost",
+        'HOST': 'pgdb',
+        # "HOST": "localhost",
         "PORT": "5432",
     }
 }
